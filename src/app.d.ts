99be4ae66163eb5a97fe1d8fbfcf2b9723619ebe
Quill <redtypesw@gmail.com>
2025-10-20 22:46:33 -0400

    Add app types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,25 @@
+import type { Session, UserFull } from "$types";
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
+  type Result<T, E> = { ok: T } | { err: E };
+
   namespace App {
-    // interface Error {}
-    // interface Locals {}
-    // interface PageData {}
+    // backend-available data
+    interface Locals {
+      session: Session | null;
+      user: UserFull | null;
+    }
+
+    // frontend-available data
+    // (use optional values for data only on specific pages)
+    interface PageData {
+      session: Session | null;
+      user: UserFull | null;
+    }
+
+    interface Error {}
     // interface PageState {}
     // interface Platform {}
   }
