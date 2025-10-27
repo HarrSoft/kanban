849b406164291d6e112a8fa4bab573c2005558c2
Quill <redtypesw@gmail.com>
2025-10-27 18:55:19 -0400

    Figured out themer

--- src/lib/components/layout/Themer.svelte
+++ src/lib/components/layout/Themer.svelte
@@ -6,72 +6,72 @@
 
 {#if theme === "burning-love"}
   <style>
-    :global {
-      :root {
-        --main: #1c1f33;
-        --shadow: #666370;
-        --alt: #9b7874;
-        --hilight: #d33e43;
-        --text: #ff220c;
-      }
+    :root {
+      --main: #1c1f33;
+      --shadow: #666370;
+      --alt: #9b7874;
+      --hilight: #d33e43;
+      --print: #ff220c;
     }
   </style>
 {:else if theme === "oceanside"}
   <style>
-    :global {
-      :root {
-        --main: #d8dbe2;
-        --shadow: #a9bcd0;
-        --alt: #58a4b0;
-        --hilight: #daa49a;
-        --text: #373f51;
-      }
+    :root {
+      --main: #d8dbe2;
+      --shadow: #a9bcd0;
+      --alt: #58a4b0;
+      --hilight: #daa49a;
+      --print: #373f51;
     }
   </style>
 {:else if theme === "ribbit-dark"}
   <style>
-    :global {
-      :root {
-        --main: #34403a;
-        --shadow: #285238;
-        --alt: #138a36;
-        --hilight: #04e824;
-        --text: #18ff6d;
-      }
+    :root {
+      --main: #34403a;
+      --shadow: #285238;
+      --alt: #138a36;
+      --hilight: #04e824;
+      --print: #18ff6d;
     }
   </style>
 {:else if theme === "ribbit-light"}
   <style>
-    :global {
-      :root {
-        --main: #18ff6d;
-        --shadow: #04e824;
-        --alt: #138a36;
-        --hilight: #285238;
-        --text: #34403a;
-      }
+    :root {
+      --main: #18ff6d;
+      --shadow: #04e824;
+      --alt: #138a36;
+      --hilight: #285238;
+      --print: #34403a;
     }
   </style>
 {:else if theme === "auto"}
   <style>
-    :global {
-      :root {
-        /* default light mode: oceanside */
-        --main: #d8dbe2;
-        --shadow: #a9bcd0;
-        --alt: #58a4b0;
-        --hilight: #daa49a;
-        --text: #373f51;
-      }
+    :root {
+      /* default light mode: oceanside */
+      --main: #d8dbe2;
+      --shadow: #a9bcd0;
+      --alt: #58a4b0;
+      --hilight: #daa49a;
+      --print: #373f51;
+    }
 
-      @media (prefers-color-scheme: dark) {
+    @media (prefers-color-scheme: dark) {
+      :root {
         /* default dark mode: burning-love */
         --main: #1c1f33;
         --shadow: #666370;
         --alt: #9b7874;
         --hilight: #d33e43;
-        --text: #ff220c;
+        --print: #ff220c;
+        --invert: #550003;
       }
     }
   </style>
 {/if}
+
+<style>
+  :root {
+    background-color: var(--main);
+    color: var(--print);
+  }
+</style>
