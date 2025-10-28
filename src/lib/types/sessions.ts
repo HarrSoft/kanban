7affac34c47f3ff16d8e5683da61d7502fe7cf3d
Quill <redtypesw@gmail.com>
2025-10-27 21:14:30 -0400

    Remove DBSession type, app does not care when token expires

--- src/lib/types/sessions.ts
+++ src/lib/types/sessions.ts
@@ -2,17 +2,12 @@ import { z } from "zod";
 import { PlatformRole, SessionId, UserHandle, UserId } from "./ids";
 import { Theme } from "./themes";
 
-export const DBSession = z.object({
+export const Session = z.object({
   sessionId: SessionId,
   userId: UserId,
   userHandle: UserHandle,
   platformRole: PlatformRole,
-  sessionExpiresAt: z.date(),
+  expiresAt: z.date(),
   theme: Theme,
-});
-export type DBSession = z.infer<typeof DBSession>;
-
-export const Session = DBSession.extend({
-  tokenExpiresAt: z.date(),
 });
 export type Session = z.infer<typeof Session>;
