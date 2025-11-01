a726fbc27a71e4ac4157db6852f777aacfacfe07
Quill <redtypesw@gmail.com>
2025-11-01 17:56:58 -0400

    Fix isUserOrAdmin rule

--- src/routes/api/rules.ts
+++ src/routes/api/rules.ts
@@ -1,6 +1,6 @@
 import { error } from "@sveltejs/kit";
 import { getRequestEvent } from "$app/server";
-import { UserHandle } from "$types";
+import { UserIdentity } from "$types";
 
 export const isLoggedIn = () => {
   const event = getRequestEvent();
@@ -10,13 +10,20 @@ export const isLoggedIn = () => {
   }
 };
 
-export const isUserOrAdmin = (handle: UserHandle) => {
+export const isUserOrAdmin = (ident: UserIdentity) => {
   const event = getRequestEvent();
+  const session = event.locals.session;
 
-  if (
-    event.locals.session?.platformRole !== "admin" &&
-    handle !== event.locals.session?.userHandle
-  ) {
+  if (!session) {
+    throw error(401);
+  }
+
+  const isUser =
+    ident.id === session.userId ||
+    ident.handle === session.userHandle ||
+    ident.email === session.userEmail;
+
+  if (!isUser && session.platformRole !== "admin") {
     throw error(401);
   }
 };
