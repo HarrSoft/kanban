f26ba40d2d8a0835ea7eb6cd06387c5fb73a159b
Quill <redtypesw@gmail.com>
2026-01-05 16:35:31 -0500

    Scaffold admin section

---
+++ src/routes/(admin)/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import { Nav } from "$com/layout";
+  import type { LayoutData } from "./$types";
+
+  const {
+    children,
+    //data,
+  }: {
+    children?: Snippet;
+    data: LayoutData;
+  } = $props();
+</script>
+
+<div class={["flex h-full w-full flex-col", "lg:flex-row"]}>
+  <Nav
+    tabs={[
+      { path: "/projects", name: "Projects" },
+      { path: "/users", name: "Users" },
+    ]} />
+  <div class="h-full w-full overflow-y-auto p-4">
+    {@render children?.()}
+  </div>
+</div>
