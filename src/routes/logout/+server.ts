100d06d10dc42e70e9fde38403cae91ee1d6b3f6
Quill <redtypesw@gmail.com>
2026-01-06 11:50:01 -0500

    Implement /logout

---
+++ src/routes/logout/+server.ts
@@ -0,0 +1,14 @@
+import { redirect } from "@sveltejs/kit";
+import db from "$db";
+import { deleteSessionTokenCookie } from "$server/auth/cookie";
+import { invalidateSession } from "$server/auth/session";
+import type { RequestHandler } from "./$types";
+
+export const GET: RequestHandler = async ({ locals }) => {
+  if (locals.session) {
+    await invalidateSession(db, locals.session.sessionId);
+    deleteSessionTokenCookie();
+  }
+
+  return redirect(303, "/");
+}
