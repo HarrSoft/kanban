93159534981b656f63508da12a56806383b5e139
Quill <redtypesw@gmail.com>
2026-01-06 11:44:18 -0500

    Implement login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,26 +1,61 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+  import { login } from "./login.remote";
+
   let email = $state("");
   let password = $state("");
+  let error = $state("");
 
-  const doLogin = () => {};
+  let doingLogin = false;
+  const doLogin = async () => {
+    if (doingLogin) {
+      return;
+    }
+    doingLogin = true;
+
+    try {
+      await login({ email, password });
+    } catch {
+      error = "Credentials are incorrect";
+      doingLogin = false;
+      return;
+    }
+
+    doingLogin = false;
+    goto("/project");
+  };
 </script>
 
 <div class="m-4 grid grid-cols-[5rem_15rem] items-center gap-2">
   <h1 class="col-span-2 font-bold">Login</h1>
+
+  {#if error}
+    <span class="text-red-500 col-span-2">
+      Error: {error}
+    </span>
+  {/if}
 
   <label for="email">Email</label>
   <input
     name="email"
     type="email"
     bind:value={email}
-    class="rounded-md bg-transparent" />
+    class="rounded-md bg-transparent"
+    oninput={() => {
+      error = "";
+    }}
+  />
 
   <label for="password">Password</label>
   <input
     name="password"
     type="password"
     bind:value={password}
-    class="rounded-md bg-transparent" />
+    class="rounded-md bg-transparent"
+    oninput={() => {
+      error = "";
+    }}
+  />
 
   <button
     type="submit"
@@ -29,7 +64,9 @@
       "rounded-md border-2 px-4 py-2",
       "hover:bg-[var(--shadow)]",
       "col-span-2 font-bold",
-    ]}>
+    ]}
+    disabled={!email || !password}
+  >
     Submit
   </button>
 </div>
