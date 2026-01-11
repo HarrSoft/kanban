0e5e79672f79a05af86d5e982bb68163f607db87
Quill <redtypesw@gmail.com>
2026-01-11 13:30:49 -0500

    Add skeletal project page

---
+++ src/routes/(user)/project/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  const {
+    //data,
+  }: {
+    data: PageData;
+  } = $props();
+</script>
+
+<div class="m-auto grid grid-cols-2"></div>
