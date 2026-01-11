53f20ea74ddc4b54ee90558b34f322fed0a903c7
Quill <redtypesw@gmail.com>
2026-01-11 15:13:23 -0500

    Added project create link

--- src/lib/components/layout/ProjectPicker.svelte
+++ src/lib/components/layout/ProjectPicker.svelte
@@ -1,15 +1,31 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import { page } from "$app/state";
   import { pickProject } from "$lib/remote";
+  import { ProjectId } from "$types";
 
   let projectId = $state(page.data.activeProject);
+
+  // CUID2 will never produce an id containing a !
+  const NEW_PROJECT_SYM = "!NEW!" as ProjectId;
+
+  const pick = () => {
+    if (projectId === NEW_PROJECT_SYM) {
+      return goto("/project/create");
+    } else {
+      return pickProject({ projectId });
+    }
+  };
 </script>
 
-<select bind:value={projectId} onchange={() => pickProject({ projectId })}>
+<select bind:value={projectId} onchange={pick}>
   <option value={null}>-- Select a project --</option>
   {#each page.data.projects as project}
     <option value={project.id} class="flex items-center">
       {project.name}
     </option>
   {/each}
+  {#if page.data.session && page.data.session.platformRole !== "viewer"}
+    <option value={NEW_PROJECT_SYM}>+ Create new project</option>
+  {/if}
 </select>
--- src/routes/(user)/project/+page.svelte
+++ src/routes/(user)/project/+page.svelte
@@ -2,10 +2,32 @@
   import type { PageData } from "./$types";
 
   const {
-    //data,
+    data,
   }: {
     data: PageData;
   } = $props();
+
+  const project = $derived(
+    data.projects.find(p => p.id === data.activeProject),
+  );
 </script>
 
-<div class="m-auto grid grid-cols-2"></div>
+<div class="scroll-y-auto flex flex-col p-2">
+  {#if project}
+    <!-- Project title & info -->
+    <div class="grid grid-cols-2 gap-2">
+      {#if project.imageUrl}
+        <img src={project.imageUrl} alt="Project logo" class="h-20 w-20" />
+      {:else}
+        <span class="text-lg">🎁</span>
+      {/if}
+      <span class="text-lg font-bold">{project.name}</span>
+    </div>
+
+    <!-- Project members -->
+    <div class="grid grid-cols-4"></div>
+  {:else}
+    <span class="m-2 text-lg"
+      >Create a project or ask to join one to continue</span>
+  {/if}
+</div>
