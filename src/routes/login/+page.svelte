43e324f0add55d171c887c4ec8b6fcfdb95da3c6
Quill <redtypesw@gmail.com>
2026-01-22 17:08:54 -0500

    Fix bad import

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { buttonThemes } from "$com/widgets";
   import { login } from "$lib/remote";
 </script>
 
@@ -8,10 +7,7 @@
 
   <label>
     <h2>Email</h2>
-    <input
-      {...login.fields.email.as("email")}
-      class="rounded-md bg-transparent"
-    />
+    <input {...login.fields.email.as("email")} />
     {#each login.fields.email.issues() || [] as issue}
       <p class="text-red-500">{issue.message}</p>
     {/each}
@@ -19,20 +15,11 @@
 
   <label>
     <h2>Password</h2>
-    <input
-      {...login.fields._password.as("password")}
-      type="password"
-      class="rounded-md bg-transparent"
-    />
+    <input {...login.fields._password.as("password")} />
     {#each login.fields._password.issues() || [] as issue}
       <p class="text-red-500">{issue.message}</p>
     {/each}
   </label>
 
-  <button
-    type="submit"
-    class={[buttonThemes.solid.base, buttonThemes.solid.enabled]}
-  >
-    Submit
-  </button>
+  <button type="submit" class="button solid">Submit</button>
 </form>
