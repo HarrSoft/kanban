465d8060af1a1de1802b35c6f01d13b792acf657
Lavra Tamutus <lavratamutus@gmail.com>
2026-02-19 20:11:08 -0500

    Add resolve and lint

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -1,37 +1,38 @@
 <script lang="ts">
-  import { ProjectInfo } from "$types";
+	import { resolve } from '$app/paths';
+	import { ProjectInfo } from '$types';
 
-  const {
-    project,
-    name = true,
-    link = false,
-  }: {
-    project: ProjectInfo;
-    name?: boolean;
-    link?: boolean;
-  } = $props();
+	const {
+		project,
+		name = true,
+		link = false,
+	}: {
+		project: ProjectInfo;
+		name?: boolean;
+		link?: boolean;
+	} = $props();
 </script>
 
 {#if link}
-  <a href={`/project/${project.id}`} class="flex items-center gap-2">
-    {@render content()}
-  </a>
+	<a href={resolve(`/project/${project.id}`)} class="flex items-center gap-2">
+		{@render content()}
+	</a>
 {:else}
-  <div class="flex items-center gap-2">
-    {@render content()}
-  </div>
+	<div class="flex items-center gap-2">
+		{@render content()}
+	</div>
 {/if}
 
 {#snippet content()}
-  {#if project.imageUrl}
-    <img src={project.imageUrl} alt="Project Icon" class="h-15 w-15" />
-  {:else}
-    <span class="flex h-15 w-15 items-center justify-center text-[2rem]">
-      🎁
-    </span>
-  {/if}
+	{#if project.imageUrl}
+		<img src={project.imageUrl} alt="Project Icon" class="h-15 w-15" />
+	{:else}
+		<span class="flex h-15 w-15 items-center justify-center text-[2rem]">
+			🎁
+		</span>
+	{/if}
 
-  {#if name}
-    <h1 class="text-2xl font-bold">{project.name}</h1>
-  {/if}
+	{#if name}
+		<h1 class="text-2xl font-bold">{project.name}</h1>
+	{/if}
 {/snippet}
--- src/lib/components/ProjectPicker.svelte
+++ src/lib/components/ProjectPicker.svelte
@@ -1,71 +1,71 @@
 <script lang="ts">
-  import { Chevron } from "$com/icons";
-  import { getActiveProject, getProjects, pickProject } from "$lib/remote";
-  import { ProjectInfo } from "$types";
+	import { Chevron } from '$com/icons';
+	import { getActiveProject, getProjects, pickProject } from '$lib/remote';
+	import { ProjectInfo } from '$types';
 
-  const activeProject = $derived(await getActiveProject());
+	const activeProject = $derived(await getActiveProject());
 
-  // component works with pure css, but this code makes ux better
-  let justFocused = false;
+	// component works with pure css, but this code makes ux better
+	let justFocused = false;
 </script>
 
 <div class="group relative w-55 max-w-70 **:cursor-pointer">
-  <button
-    class={[
-      "w-full border bg-base p-2",
-      "flex items-center justify-between gap-2",
-      "rounded-lg group-focus-within:rounded-b-none",
-    ]}
-    onfocus={() => (justFocused = true)}
-    onblur={() => (justFocused = false)}
-    onclick={e => {
-      if (justFocused) {
-        justFocused = false;
-      } else {
-        e.currentTarget.blur();
-      }
-    }}
-  >
-    {#if activeProject}
-      {@render projectInfo(activeProject)}
-    {:else}
-      <span class="mx-auto">-- Pick a Project --</span>
-    {/if}
+	<button
+		class={[
+			'bg-base w-full border p-2',
+			'flex items-center justify-between gap-2',
+			'rounded-lg group-focus-within:rounded-b-none',
+		]}
+		onfocus={() => (justFocused = true)}
+		onblur={() => (justFocused = false)}
+		onclick={(e) => {
+			if (justFocused) {
+				justFocused = false;
+			} else {
+				e.currentTarget.blur();
+			}
+		}}
+	>
+		{#if activeProject}
+			{@render projectInfo(activeProject)}
+		{:else}
+			<span class="mx-auto">-- Pick a Project --</span>
+		{/if}
 
-    <Chevron className="group-focus-within:rotate-180" />
-  </button>
+		<Chevron className="group-focus-within:rotate-180" />
+	</button>
 
-  <form
-    {...pickProject}
-    class={[
-      "absolute w-full rounded-lg rounded-t-none bg-base",
-      "h-0 max-h-100 group-focus-within:h-auto",
-      "border-0 group-focus-within:border group-focus-within:border-t-0",
-      "overflow-y-hidden group-focus-within:overflow-y-auto",
-    ]}
-  >
-    {#each await getProjects() as project}
-      <button
-        {...pickProject.fields.projectId.as("submit", project.id)}
-        class="w-full p-2 hover:bg-shadow"
-        onclick={e => e.currentTarget.blur()}
-      >
-        {@render projectInfo(project)}
-      </button>
-    {:else}
-      <div class="w-full p-2 text-center">-- No Projects --</div>
-    {/each}
-  </form>
+	<form
+		{...pickProject}
+		class={[
+			'bg-base absolute w-full rounded-lg rounded-t-none',
+			'h-0 max-h-100 group-focus-within:h-auto',
+			'border-0 group-focus-within:border group-focus-within:border-t-0',
+			'overflow-y-hidden group-focus-within:overflow-y-auto',
+		]}
+	>
+		{#each await getProjects() as project (project.id)}
+			<button
+				{...pickProject.fields.projectId.as('submit', project.id)}
+				class="hover:bg-shadow w-full p-2"
+				onclick={(e) => e.currentTarget.blur()}
+			>
+				{@render projectInfo(project)}
+			</button>
+		{:else}
+			<div class="w-full p-2 text-center">-- No Projects --</div>
+		{/each}
+	</form>
 </div>
 
 {#snippet projectInfo(project: ProjectInfo)}
-  <div class="flex items-center gap-2">
-    {#if project.imageUrl}
-      <img src={project.imageUrl} alt="icon" class="w-[2rem]" />
-    {:else}
-      <span class="w-[2rem]">🎁</span>
-    {/if}
+	<div class="flex items-center gap-2">
+		{#if project.imageUrl}
+			<img src={project.imageUrl} alt="icon" class="w-8" />
+		{:else}
+			<span class="w-8">🎁</span>
+		{/if}
 
-    <span>{project.name}</span>
-  </div>
+		<span>{project.name}</span>
+	</div>
 {/snippet}
