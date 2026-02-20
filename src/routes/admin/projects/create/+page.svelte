0e22a3165018d4365b5802f5294d5eaee6ef1ecc
Lavra Tamutus <lavratamutus@gmail.com>
2026-02-19 20:07:43 -0500

    Fix problems that could have been detected with the use of Intellisense; apply linting rules

--- src/routes/admin/projects/create/+page.svelte
+++ src/routes/admin/projects/create/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-  import { createProject } from "$lib/remote";
+	import { createProject } from '$lib/remote';
 </script>
 
 <form {...createProject} class="flex flex-col gap-2">
-  <h1 class="text-lg font-bold">Create a new project</h1>
+	<h1 class="text-lg font-bold">Create a new project</h1>
 
-  <label>
-    <h2>Project Name</h2>
-    <input {...createProject.fields.name.as("text")} />
-    {#each createProject.fields.name.issues() || [] as issue}
-      <span class="text-red-500">{issue.message}</span>
-    {/each}
-  </label>
+	<label>
+		<h2>Project Name</h2>
+		<input {...createProject.fields.name.as('text')} />
+		{#each createProject.fields.name.issues() || [] as issue (issue.message)}
+			<span class="text-red-500">{issue.message}</span>
+		{/each}
+	</label>
 
-  <button class="button solid w-fit">Create Project</button>
+	<button class="button solid w-fit">Create Project</button>
 </form>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-  import { login } from "$lib/remote";
+	import { login } from '$lib/remote';
 </script>
 
 <form {...login} class="mx-auto flex flex-col items-center gap-2">
-  <h1 class="text-xl font-bold">Login</h1>
+	<h1 class="text-xl font-bold">Login</h1>
 
-  <label>
-    <h2>Email</h2>
-    <input {...login.fields.email.as("email")} />
-    {#each login.fields.email.issues() || [] as issue}
-      <p class="text-red-500">{issue.message}</p>
-    {/each}
-  </label>
+	<label>
+		<h2>Email</h2>
+		<input {...login.fields.email.as('email')} />
+		{#each login.fields.email.issues() || [] as issue (issue.message)}
+			<p class="text-red-500">{issue.message}</p>
+		{/each}
+	</label>
 
-  <label>
-    <h2>Password</h2>
-    <input {...login.fields._password.as("password")} />
-    {#each login.fields._password.issues() || [] as issue}
-      <p class="text-red-500">{issue.message}</p>
-    {/each}
-  </label>
+	<label>
+		<h2>Password</h2>
+		<input {...login.fields._password.as('password')} />
+		{#each login.fields._password.issues() || [] as issue (issue.message)}
+			<p class="text-red-500">{issue.message}</p>
+		{/each}
+	</label>
 
-  <button type="submit" class="button solid">Submit</button>
+	<button type="submit" class="button solid">Submit</button>
 </form>
